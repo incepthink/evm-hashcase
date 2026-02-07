f7f48d634b32c2149744d8fe14b3cdfbb90da447
incepthink <incepthink@gmail.com>
2026-02-08 03:44:11 +0530

    feat: rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    //to fix issues with fs in client components
-    config.resolve.fallback = {
-      // if you miss it, all the other options in fallback, specified
-      // by next.js will be dropped.
-      ...config.resolve.fallback,
-
-      fs: false, // the solution
-    };
-
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
-
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ["@svgr/webpack"],
-      }
-    );
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
+    },
   },
   images: {
     remotePatterns: [
@@ -86,16 +58,18 @@ const nextConfig = {
     ],
   },
   async rewrites() {
-    return [
-      {
-        source: "/minter/filecoin",
-        destination: "https://filecoin.hashcase.co/minter/filecoin",
-      },
-      {
-        source: "/minter/filecoin/:path*",
-        destination: "https://filecoin.hashcase.co/minter/filecoin/:path*",
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: "/minter/filecoin",
+          destination: "https://filecoin.hashcase.co/minter/filecoin",
+        },
+        {
+          source: "/minter/filecoin/:path*",
+          destination: "https://filecoin.hashcase.co/minter/filecoin/:path*",
+        },
+      ],
+    };
   },
 };
 
